19cbb2d403f7cfcc089fd1cd61dd6fdc65f737df
ilzat <ilzat.sadykov.2006@bk.ru>
2024-07-17 19:49:30 +0600

    'Добавил Header.tsx поднастроил проект'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Header from "./Components/Header/Header.tsx";
+import {Routes} from "react-router-dom";
 
 const App = () => {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <Routes>
+
+
+            </Routes>
+        </>
+    )
 };
 
 export default App
